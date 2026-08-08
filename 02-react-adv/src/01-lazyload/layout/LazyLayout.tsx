e8b627253fbcd5d7f5dffa2b6e2332a2921c0a79
Alejandro Cruz <alex_crz97@hotmail.com>
2026-08-08 01:01:00 -0600

    dockerize app

--- 02-react-adv/src/01-lazyload/layout/LazyLayout.tsx
+++ 02-react-adv/src/01-lazyload/layout/LazyLayout.tsx
@@ -8,13 +8,13 @@ export const LazyLayout = () => {
       <h1>LazyLayour page</h1>
       <ul>
         <li>
-          <NavLink to="lazy1">Lazy 1</NavLink>
+          <NavLink to="/lazyload/lazy1">Lazy 1</NavLink>
         </li>
         <li>
-          <NavLink to="lazy2">Lazy 2</NavLink>
+          <NavLink to="/lazyload/lazy2">Lazy 2</NavLink>
         </li>
         <li>
-          <NavLink to="lazy3">Lazy 3</NavLink>
+          <NavLink to="/lazyload/lazy3">Lazy 3</NavLink>
         </li>
       </ul>
 
@@ -22,9 +22,9 @@ export const LazyLayout = () => {
         <Route path="lazy1" element={ <LazyPage1 /> }/>
         <Route path="lazy2" element={ <LazyPage2 /> }/>
         <Route path="lazy3" element={ <LazyPage3 /> }/>
-        
+
         {/* <Route path="*" element={ <div>Not Found</div> }/> */}
-        <Route path="*" element={ <Navigate replace to ="lazy1" /> }/>
+        <Route path="*" element={ <Navigate replace to ="/lazyload/lazy1" /> }/>
       </Routes>
       { /* Rutas hijas iran aqui*/ }
     </div>
